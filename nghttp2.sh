e9f3757923ad3be078694661b54cf7515efb702f
Javier Gusano Martínez <jgm1986@users.noreply.github.com>
2015-12-04 10:49:12 +0100

    Installation script to the new nghttp2 version.
    Nghttp2 1.5.0

--- nghttp2.sh
+++ nghttp2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "********************************************************"
-echo "* NGHTTP2: 1.3.2                                       *"
+echo "* NGHTTP2: 1.5.0                                       *"
 echo "********************************************************"
 sudo apt-get install make binutils autoconf  automake autotools-dev libtool pkg-config \
   zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev \
@@ -16,9 +16,9 @@ sudo apt-get install    gcc         \
             cython          \
             python3.4-dev       \
             -y
-wget https://github.com/tatsuhiro-t/nghttp2/releases/download/v1.3.2/nghttp2-1.3.2.tar.gz --no-check-certificate
-tar xfv nghttp2-1.3.2.tar.gz
-cd nghttp2-1.3.2/
+wget https://github.com/tatsuhiro-t/nghttp2/releases/download/v1.5.0/nghttp2-1.5.0.tar.gz --no-check-certificate
+tar xfv nghttp2-1.5.0.tar.gz
+cd nghttp2-1.5.0/
 export PKG_CONFIG_PATH=/usr/local/ssl/lib/pkgconfig
 export LD_LIBRARY_PATH=/usr/local/ssl/lib
 autoreconf -i
